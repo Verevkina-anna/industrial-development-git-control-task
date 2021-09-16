5abf0f0854c8c166b6d5d494340efa4cb4eee874
Verevkina <verevkina-anna1998@mail.com>
2021-09-16 16:22:36 +0300

    новый перенос

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,5 @@ public class HelloWorld{
 public static void
 main(String[] args){
 System.out.println("Hello, World!\n");
-System.out.println("New Feature!");}
+System.out.println("New Feature!\n");}
 }
